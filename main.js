7636148ba38e238587562c5fec3c820b106bfc31
Apple <apple@Apples-MacBook-Pro-26.local>
2019-08-14 16:43:51 -0400

    title page

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-regea
+var button = $('.submit');
+var genre = $('.drop-down');
+var subgenre = $('.drop-down-two');
+var rating = $('.drop-down-three');
+var empty = $('.acceptinformation');
+
+button.on("click", allInformation);
+
+function allInformation() {
+  event.preventDefault();
+  var genre = $('.drop-down').val();
+  var subgenre = $('.drop-down-two').val();
+  var rating = $('.drop-down-three').val();
+  empty.append(`you picked a ${rating} ${genre} ${subgenre} movie!`);
+  $('.drop-down').val("");
+  $('.drop-down-two').val("");
+  $('.drop-down-three').val("");
+}
